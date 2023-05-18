6a54f71833af86830f08a7d2274e02c6c160312a
yunyj1998 <yunyj1998@gmail.com>
2023-05-18 16:24:24 +0900

    update

--- app.py
+++ app.py
@@ -3,32 +3,22 @@ import streamlit as st
 from PIL import Image
 
 # Confit
-st.set_page_config(page_title='Cross Chain Monitoring Tool', page_icon=':bar_chart:', layout='wide')
+st.set_page_config(page_title='YYJ Test ', page_icon=':bar_chart:', layout='wide')
 
 # Title
-st.title('Cross Chain Monitoring Tool')
+st.title("잡다한 테스트 대시보드")
 
 # Content
-c1, c2 = st.columns(2)
+c1, c2, c3 = st.columns(3)
 c1.image(Image.open("images/1.png"))
 c2.image(Image.open("images/2.png"))
-
+c3.image(Image.open("images/3.png"))
 
 st.write(
     """
-    The crypto industry continues to progress and its development has never stopped. Contributors
-    of each blockchain keep developing each segment of the industry and the whole crypto ecosystem.
-    This tool is designed to allow viewers to journey into the world of crypto ecosystems of some
-    of the major blockchains, and compare their performance.
+    아무글 이나 일단  써보는 ***대시보드*** 테스트 페이지 이다.
+    이페이지는 메인 페이지가 될것이며 꾸미는것은  어떻게 될지 아직 모르는상태.
 
-    This tool is designed and structured in multiple **Pages** that are accessible using the sidebar.
-    Each of these Pages addresses a different segment of the crypto industry. Within each segment
-    (Macro, Transfers, Swaps, NFTs, etc.) you are able to filter your desired blockchains to
-    narrow/expand the comparison. By selecting a single blockchain, you can observe a deep dive
-    into that particular network.
-
-    All values for amounts, prices, and volumes are in **U.S. dollars** and the time frequency of the
-    analysis was limited to the last **30 days**.
     """
 )
 
